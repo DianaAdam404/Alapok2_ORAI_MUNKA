08df4bcf36d1316ee9597edbb899ef97bc42e63e
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-14 11:17:02 +0200

    3.Feladat

---
+++ FelhasznaloFiok/Program.cs
@@ -0,0 +1,13 @@
+﻿using FelhasznaloFiok;
+Felhasznalo hasznalo = new Felhasznalo { Felhasznalonev = "test" };
+//hasznalo.jelszo = "nemmukodik";
+//       ^^^jelszo privalt^^^
+
+Console.WriteLine("Hibás régi jelszó teszt");
+hasznalo.JelszoModositas("rossz123", "ujJelszo123");
+
+Console.WriteLine("Tul rövid uj jelszó test");
+hasznalo.JelszoModositas("123456", "123");
+
+Console.WriteLine("Helyes módosítás test ---");
+hasznalo.JelszoModositas("123456", "ujJelszo123");
