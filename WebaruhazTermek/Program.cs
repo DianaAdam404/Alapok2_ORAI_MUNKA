2b269c333aa6aac9ea2b8b9d21ac868c1bbb0948
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-14 10:28:04 +0200

    Add project files.

---
+++ WebaruhazTermek/Program.cs
@@ -0,0 +1,7 @@
+﻿using WebaruhazTermek;
+
+Termek termek1 = new Termek { Nev = "Laptop", Egysegar = 250000, Keszlet = 10 };
+Termek termek2 = new Termek { Nev = "Gep", Egysegar = -500, Keszlet = -5 };
+
+Console.WriteLine($"{termek1.Nev} Ár: {termek1.Egysegar} Ft, Készlet: {termek1.Keszlet} db.");
+Console.WriteLine($"{termek2.Nev} Ár: {termek2.Egysegar} Ft, Készlet: {termek2.Keszlet} db.");
---
+++ WebaruhazTermek/Termek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace WebaruhazTermek
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+        private int egysegar;
+        public int Egysegar
+        {
+            get { return egysegar; }
+            set 
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("Az egységár nem lehet kisebb, mint 1.");
+                    egysegar = 1;
+                }
+                else egysegar = value;
+            }
+        }
+
+        private int keszlet;
+        public int Keszlet
+        {
+            get { return keszlet; }
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("A készlet nem lehet negatív.");
+                    keszlet = 0;
+                }
+                else keszlet = value;
+            }
+        }
+    }
+
+}
+}
