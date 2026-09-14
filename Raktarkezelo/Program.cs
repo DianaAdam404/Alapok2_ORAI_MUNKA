caf0c43275540dbbd715bb2b57553c6952080716
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-14 11:44:19 +0200

    4.feladat

---
+++ Raktarkezelo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Raktarkezelo;
+RaktarElem elem = new RaktarElem { CikkNev = "Iphone" };
+Console.WriteLine($"--{elem.CikkNev} raktárkezelése--");
+Console.Write("Kérem a bevételezett mennyiséget: ");
+elem.db = int.Parse(Console.ReadLine());
+elem.Bevetelezes(elem.db);
+
+Console.Write("Kérem a kiadott mennyiséget: ");
+elem.db = int.Parse(Console.ReadLine());
+elem.Kiadas(elem.db);
+
+double nettoAr = 15000;
+double brutto = RaktarElem.BruttoArSzamitas(nettoAr);
+Console.WriteLine($"Nettó ár: {nettoAr}Ft = Bruttó ár: {brutto}Ft");
---
+++ Raktarkezelo/RaktarElem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raktarkezelo
+{
+    internal class RaktarElem
+    {
+        private int dbSzam;
+        public string CikkNev { get; set; }
+        public static double AfaKulcs { get; set; } = 0.27;
+        public int db;
+        public void Bevetelezes(int db)
+        {
+            if (db <= 0)
+            {
+                Console.WriteLine("Figyelmeztetés: A bevételezett mennyiségnek 0-nál nagyobbnak kell lennie!");
+                return;
+            }
+            dbSzam += db;
+            Console.WriteLine($"Bevételezve: {db} db. Jelenlegi készlet: {dbSzam} db.");
+        }
+        public void Kiadas(int db)
+        {
+            if (db <= 0)
+            {
+                Console.WriteLine("Figyelmeztetés: A kiadott mennyiségnek 0-nál nagyobbnak kell lennie!");
+                return;
+            }
+            if (db > dbSzam)
+            {
+                Console.WriteLine("Figyelmeztetés: Nincs ennyi készlet!");
+                return;
+            }
+            dbSzam -= db;
+            Console.WriteLine($"Kiadva: {db} db. Jelenlegi készlet: {dbSzam} db.");
+        }
+        public static double BruttoArSzamitas(double nettoAr)
+        {
+            return nettoAr * (1 + AfaKulcs);
+        }
+    }
+}
