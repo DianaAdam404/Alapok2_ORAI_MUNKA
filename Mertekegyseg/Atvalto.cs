45833cf2ddd9117dbdb1c5c9d8bae2e24bbb2932
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-14 11:01:37 +0200

    2.Feladat

---
+++ Mertekegyseg/Atvalto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mertekegyseg
+{
+    internal class Atvalto
+    {
+        public double ertek;
+        public double KmBolMeter(double ertek)
+        {
+            return ertek * 1000;
+        }
+        public double Szorzo;
+        public double EgyediAtvaltas(double ertek)
+        {
+            return ertek * Szorzo;
+        }
+    }
+}
---
+++ Mertekegyseg/Program.cs
@@ -0,0 +1,13 @@
+﻿using Mertekegyseg;
+using System.Security.Cryptography;
+
+Atvalto atvalto = new Atvalto();
+Console.Write("Kérem a km értéket: ");
+atvalto.ertek = double.Parse(Console.ReadLine());
+Console.WriteLine($"{atvalto.ertek}km = {atvalto.KmBolMeter(atvalto.ertek)}m.");
+
+Console.Write("Kérem az átváltási szorzót: ");
+atvalto.Szorzo = double.Parse(Console.ReadLine());
+Console.Write("Kérem az új km értéket: ");
+atvalto.ertek = double.Parse(Console.ReadLine());
+Console.WriteLine($"{atvalto.ertek}km = {atvalto.EgyediAtvaltas(atvalto.ertek)}m.");
